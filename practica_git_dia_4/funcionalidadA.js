cfdbe3de540f7db5634ce51880c69041d8aa6173
Cap <gestion2008cap@hotmail.com>
2025-11-10 20:43:22 -0300

    feat: se corrige error por cierre de llaves

--- practica_git_dia_4/funcionalidadA.js
+++ practica_git_dia_4/funcionalidadA.js
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 // Se agrega funcion A
 function funcionA() {
     return 'Hola - rama feature/funcionalidad-A - versión A';
-=======
 // Se agrega funcion B
-function funcionA() {
+function funcionAb() {
     return 'rama feature/funcionalidad-B - Versión A';
->>>>>>> feature/funcionalidad-B
+}
 }
